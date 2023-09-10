a41120827fd9a2fce5ee1e6dee6e83273b4aa82a
Breedwizzy <breedwizzy@yahoo..com>
2023-09-10 16:03:04 +0100

    API Consumed

--- wizzy-create-customblock.php
+++ wizzy-create-customblock.php
@@ -6,12 +6,13 @@
 
 function wizzycreate_custom_block_script_register()
 {
+    // Enqueue Gutenberg block editor scripts and dependencies
+    wp_enqueue_script('wp-blocks'); // Blocks
+    wp_enqueue_script('wp-i18n'); // Internationalization
+    wp_enqueue_script('wp-editor'); // Editor
+
     // Enqueue the JavaScript file for your custom block
     wp_enqueue_script('wizzy-custom-block', plugin_dir_url(__FILE__) . 'wizzycreat-custom-block.js', array('wp-blocks', 'wp-i18n', 'wp-editor'), true, false);
 }
 add_action('enqueue_block_editor_assets', 'wizzycreate_custom_block_script_register');
-
-
-
-
 ?>
--- wizzycreat-custom-block.js
+++ wizzycreat-custom-block.js
@@ -1,6 +1,170 @@
-/*import { jsx as _jsx } from "react/jsx-runtime";
-import { jsxs as _jsxs } from "react/jsx-runtime";*/
+const { registerBlockType } = wp.blocks;
+const { useState, useEffect } = wp.element;
 
+wp.blocks.registerBlockType('wizzycreat/custom-blockkkeyy', {
+    title: 'Custom Greeting Block',
+    icon: 'vault',
+    category: 'design',
+
+    attributes: {
+        greeting: {
+            type: 'string',
+        },
+    },
+
+    edit: function (props) {
+        console.log('Edit function called');
+
+        const { setAttributes } = props;
+        const [apiData, setApiData] = useState({ greeting: '' });
+
+        useEffect(() => {
+            // Fetch data from the API when the block is mounted
+            fetch('https://jsonplaceholder.typicode.com/posts/1')
+                .then(response => response.json())
+                .then(data => {
+                    // Update the state with the API data
+                    setApiData({ greeting: data.body });
+                    // Save the API data to the block attributes
+                    setAttributes({ greeting: data.body });
+                })
+                .catch(error => console.error(error));
+        }, []);
+
+        // Render the block with a placeholder
+        return wp.element.createElement('div', null,
+            wp.element.createElement('h3', null, apiData.greeting)
+        );
+    },
+
+    save: function (props) {
+        const { greeting } = props.attributes;
+
+        // Log the greeting for debugging
+        console.log('Greeting Attribute:', greeting);
+
+        // Ensure that the greeting attribute has a value
+        if (greeting) {
+            return wp.element.createElement('div', null,
+                wp.element.createElement('h3', null, greeting)
+            );
+        } else {
+            // Provide a default content in case greeting is empty
+            return wp.element.createElement('div', null, 'No greeting available');
+        }
+    },
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+This ccode is still correct with user input
+const { registerBlockType } = wp.blocks;
+const { useState, useEffect } = wp.element;
+
+wp.blocks.registerBlockType('wizzycreat/custom-blockkkeyy', {
+    title: 'Custom Greeting Block',
+    icon: 'vault',
+    category: 'design',
+
+    attributes: {
+        greeting: {
+            type: 'string',
+        },
+    },
+
+    edit: function (props) {
+        function updateGreeting(event) {
+            props.setAttributes({ greeting: event.target.value });
+        }
+
+        useEffect(() => {
+            // Fetch data when the component mounts
+            fetch('http://localhost:8000/core/homepage/')
+                .then(response => response.json())
+                .then(data => {
+                    props.setAttributes({
+                        greeting: data.greeting,
+                    });
+                })
+                .catch(error => console.error(error));
+        }, []); // The empty dependency array ensures this effect runs only once
+
+        // Render the block with an input field
+        return wp.element.createElement('div', null,
+            wp.element.createElement('label', null, 'Greeting'),
+            wp.element.createElement('input', {
+                type: 'text',
+                value: props.attributes.greeting,
+                placeholder: 'Greeting',
+                onChange: updateGreeting
+            })
+        );
+    },
+
+    save: function (props) {
+        return wp.element.createElement('div', null,
+            wp.element.createElement('h3', null, props.attributes.greeting)
+        );
+    },
+});
+
+
+
+
+
+/*
+Second Best
+
+wp.blocks.registerBlockType('wizzycreat/custom-blockkkeyy', {
+    title: 'Custom Greeting Block',
+    icon: 'vault',
+    category: 'design',
+
+    attributes: {
+        greeting: {
+            type: 'string',
+        },
+    },
+
+    edit: function (props) {
+
+        function updateFirstName(event) { props.setAttributes({ greeting: event.target.value }) }
+        return React.createElement('div', null,
+            React.createElement('label', null, 'firstName'),
+            React.createElement('input', {
+                type: 'text',
+                value: props.attributes.greeting,
+                placeholder: 'firstName',
+                onChange: updateFirstName
+            })
+        )
+    },
+
+    save: function (props) {
+
+        return React.createElement('div', null,
+            React.createElement('h3', null, props.attributes.greeting));
+    },
+});
+
+
+
+
+
+
+/*import { jsx as _jsx } from "react/jsx-runtime",
+*/
 
 
 
@@ -135,23 +299,57 @@ wp.blocks.registerBlockType('wizzzzzzycreat/reaete-custom-blockkKEY', {
 */
 
 
+
+
+/* The real one
+const { registerBlockType } = wp.blocks;
+const { TextControl, Button } = wp.components;
+const { useState } = wp.element;
+
 wp.blocks.registerBlockType('wizzycreat/custom-blockkkeyy', {
     title: 'Company Contact Information',
     icon: 'vault',
     category: 'design',
     attributes: {
-        companyName: { type: 'string' },
-        companyPhone: { type: 'string' }
+        greeting: { type: 'string' },
     },
-
     edit: function (props) {
-        return
+        const [greeting, setGreeting] = useState(props.attributes.greeting);
+
+        const fetchGreeting = () => {
+            fetch('http://localhost:8000/core/homepage/')
+                .then(response => response.json())
+                .then(data => setGreeting(data.greeting))
+                .catch(error => console.error(error));
+        };
+
+        return wp.element.createElement(
+            'div',
+            null,
+            wp.element.createElement(TextControl, {
+                label: 'Custom Greeting',
+                value: greeting,
+                onChange: setGreeting,
+            }),
+            wp.element.createElement(Button, { onClick: fetchGreeting }, 'Fetch Greeting')
+        );
     },
 
     save: function (props) {
-        return null;
+        return React.createElement(
+            'div',
+            null,
+            React.createElement(
+                'h3',
+                null,
+                props.attributes.greeting
+            )
+        );
     }
+
 });
+
+
 
 
 /*
